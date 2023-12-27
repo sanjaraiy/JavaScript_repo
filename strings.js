8b80fcf14a5cf6885c8abcfdb69f02c0e1dfac64
sanjaraiy <deepakrai78788@gmail.com>
2023-12-27 19:56:13 +0530

    strings-Methods

---
+++ strings.js
@@ -0,0 +1,90 @@
+// const name="Sanjh"
+// const repoCount=50
+
+// console.log(name + repoCount + "value");
+// console.log(`Hello my name is ${name} and repoCount is ${repoCount} value`);
+
+// const gamename=new String('Sanjh-rai');
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// // console.log(gamename.fontcolor());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('n'));
+
+// const newString=gamename.substring(0,4);
+// console.log(newString);
+// const anotherString=gamename.slice(-6,4);
+// console.log(anotherString);
+
+// const newStringOne="    Sanjh   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url="https://Sanjh.com/sanjh%20choudhary"
+
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('sundar'));
+// console.log(gamename.split('-'));
+
+//--------------------diff btw constructor string V/S normal String------------------------
+// const str1=new String("Hello")
+// const str2=String("Hello")
+// console.log(str1 instanceof String);
+// console.log(str2 instanceof String);
+// console.log(typeof str1);
+// console.log(typeof str2);
+
+//------------string method use on google-console par ja kar practise karo------------
+
+
+//********String methods***********
+const name="Hello-world"
+
+// console.log(name.anchor("heading"));
+// console.log(name.at(5));
+// console.log(name.big());
+// console.log(name.blink())
+// console.log(name.bold());
+// console.log(name.charAt(3));
+// console.log(name.charCodeAt(3)); //return decimal value of char
+// console.log(name.codePointAt(1)); //return decimal value of char
+// console.log(name.concat(" Sanjh"));
+// console.log(name.constructor());
+// console.log(name.endsWith('d'));
+// console.log(name.fixed());
+// console.log(name.fontcolor("red"));
+// console.log(name.fontsize("33px"));
+// console.log(name.includes('H'));
+// console.log(name.indexOf('w'));
+// console.log(name.lastIndexOf('w'));
+// console.log(name.isWellFormed());
+// console.log(name.italics());
+// console.log(name.valueOf());
+// console.log(name.trimLeft());
+// console.log(name.trimRight());
+// console.log(name.trimStart());
+// console.log(name.trimEnd());
+// console.log(name.toLocaleLowerCase());
+// console.log(name.toLocaleUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.toString());
+// console.log(name.sup());
+// console.log(name.substring(0,3));
+// console.log(name.substr(0,5));
+// console.log(name.startsWith('h'));
+// console.log(name.split('l'));
+// console.log(name.small());
+// console.log(name.slice(3));
+// console.log(name.search('world'));
+// console.log(name.match('world'))
+// console.log(name.repeat(2));
+// console.log(name.replace('world','sanjh'));
+// console.log(name.replaceAll('world','sanjh'));
+// console.log(name.normalize());
+
+
+
+
