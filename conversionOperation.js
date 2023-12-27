722b35ee30c28ab2497c41e760401145a496a7fe
sanjaraiy <deepakrai78788@gmail.com>
2023-12-27 12:44:15 +0530

    conversion_operators

--- conversionOperation.js
+++ conversionOperation.js
@@ -21,4 +21,37 @@ let booleamIsLoggedIn=Boolean(isLoggedIn)
 
 let someNumber=33
 let numToString=String(someNumber);
-console.log(typeof numToString);
+// console.log(numToString);
+// console.log(typeof numToString);
+
+
+//************************Operations********************************/
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+let str1="Sanjh"
+let str2=" singh"
+// console.log(str1+str2);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2); after string number convert into string
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
