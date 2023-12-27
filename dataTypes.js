bacea92f9f1411ac45b2b6721dac881c18cbc03e
sanjaraiy <deepakrai78788@gmail.com>
2023-12-27 11:15:48 +0530

    intro_of_dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+// console.log(3+3);
+
+
+//---------------two documentation-----------------
+// 1) mdn JS
+// 2) tc39 for JS
+
+
+let name="sanjh"
+let age=18;
+let isLoggedIn=false;
+
+// number=>2 to power 53
+// bigint
+// string=>""
+// booleam => true/false
+// null => standalone value
+// underfined => not value assign
+// symbol => unique
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+console.log(typeof Number)
+console.log(typeof String)
+console.log(typeof age)
+console.log(typeof isLoggedIn);
+console.log(typeof name);
+
+
+
