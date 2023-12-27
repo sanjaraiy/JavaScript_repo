9c524edf078e4793a1d911d6594c44825a4905f7
sanjaraiy <deepakrai78788@gmail.com>
2023-12-28 00:57:42 +0530

    Introduction_Of_date_Methods

---
+++ datesInJs.js
@@ -0,0 +1,31 @@
+
+// let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(typeof mydate);
+
+// let myCreatedDate=new Date(2023, 0, 23)
+// let myCreatedDate=new Date(2023, 0, 23,5,3)//(y,m,d,h,m)
+// let myCreatedDate=new Date("2023-01-14") //yyyy-mm-dd
+// let myCreatedDate=new Date("01-14-2023") //mm-dd-yyyy --> dd-mm-yyyy
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate=new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+// newDate.toLocaleString('default',{
+//     weekday:"long",
+//     // timeZone:''
+// })
+
