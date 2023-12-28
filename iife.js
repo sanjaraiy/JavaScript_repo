c26c062ac16526dbc74789c4868ac50ac51b3bd7
sanjaraiy <deepakrai78788@gmail.com>
2023-12-29 00:51:28 +0530

    intro of IIFE

---
+++ iife.js
@@ -0,0 +1,21 @@
+//Immediately Invoked Function Expressions(IIFE)
+//------Normal function-------
+// function chai(){
+//     console.log(`DB Connected...`);
+// }
+// chai();
+
+//-------IIFE function--------- 
+//********()()=>first parathensis function definition and second function execution*********
+// (function chai(){
+    //***named IIFE***
+//     console.log(`DB Connected...`);
+// })();
+
+// ((name)=>{
+    //***without named IIFE***
+//     console.log(`DB connected TWO...${name}`);
+// })('Sanjh');
+
+
+
