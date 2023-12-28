21afeba580e4f6441a9e077308088497086cf4a0
sanjaraiy <deepakrai78788@gmail.com>
2023-12-28 18:14:44 +0530

    intro_method_Of_object

--- objects.js
+++ objects.js
@@ -2,18 +2,18 @@
 //constructor se always object singleton banta hai
 
 //-----object literals-------
-const mySym=Symbol("key1")
-const User={
-      name:"Sanjh",
-      "full name":"Sanjh Rai",
-       mySym:"mykey1", //treat as string not symbol
-       [mySym]:"mykey1",
-      age:18,
-      location:"Jaipur",
-      email:"Sanjh@google.com",
-      isLoggedIn:false,
-      lastLoginDays:["Monday","Saturday"]
-}
+// const mySym=Symbol("key1")
+// const User={
+//       name:"Sanjh",
+//       "full name":"Sanjh Rai",
+//        mySym:"mykey1", //treat as string not symbol
+//        [mySym]:"mykey1",
+//       age:18,
+//       location:"Jaipur",
+//       email:"Sanjh@google.com",
+//       isLoggedIn:false,
+//       lastLoginDays:["Monday","Saturday"]
+// }
 
 // console.log(User.email);
 // console.log(User["email"]);
@@ -41,3 +41,58 @@ const User={
 // }
 // console.log(User.greetingTwo());
 
+
+
+
+
+//--------object Constructor(singleton)--------
+// const tinderUser=new Object() //singleton object
+const tinderUser={}  //non-singleton object
+ tinderUser.id="123abc"
+ tinderUser.name="Sammy"
+ tinderUser.isLoggedIn=false
+// console.log(tinderUser);
+
+const regularUser={
+    email:"some@google.com",
+    fullname:{
+        userfullname:{
+            firstname:"Sanjh",
+            lastname:"Rai"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"a",4:"b"}
+// const obj4={5:"a",6:"b"}
+// const obj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2,obj4)
+
+// const obj5={...obj1,...obj2}
+// console.log(obj5);
+
+
+const users=[
+    {
+        id:1,
+        email:"s@gmail.com"
+    },
+    {
+        id:2,
+        email:"d@gmail.com"
+    },
+    {
+        id:3,
+        email:"n@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLogged'));
