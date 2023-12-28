8a979168dca7c8f90a4ff5437b00f413e1fbbb8c
sanjaraiy <deepakrai78788@gmail.com>
2023-12-28 23:38:50 +0530

    passed array & object into function

--- functions.js
+++ functions.js
@@ -20,14 +20,50 @@
 // console.log(res);
 
 
-function loginUserMessage(username="John"){
-    if(!username){
-         console.log(`Please enter a username..!!`);
-    }else{
-        return `${username} just logged in`
-    }
-    
-}
-const res=loginUserMessage()
+// function loginUserMessage(username="John"){
+//     if(!username){
+//          console.log(`Please enter a username..!!`);
+//     }else{
+//         return `${username} just logged in`
+//     }
+// }
+// const res=loginUserMessage()
 // const res=loginUserMessage("Sanjh")
-console.log(res);
+// console.log(res);
+
+// function calculateCartPrice(val1,val2,...num1){
+//    return num1
+// }
+
+// const res=calculateCartPrice(2,4,5,6,8)
+// console.log(res);
+
+//--------passed object in function----------
+// const user={
+//     username:"Sanjh",
+//     prices:199
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+
+// // handleObject(user);
+// handleObject({
+//     username:"John",
+//     price:199
+// })
+
+
+//--------passed array in function------------
+// const arr=[200,400,100,600]
+// function returnSecondValue(anyArray){
+//  return anyArray[1]
+// } 
+
+// const res=returnSecondValue(arr);
+// console.log(res);
+// console.log(returnSecondValue([200,300,400,500]))
+
+
+
