3862241189219bb0f34517ab44715e70c556fb09
sanjaraiy <deepakrai78788@gmail.com>
2023-12-28 23:53:54 +0530

    scopes of functions

---
+++ scopes.js
@@ -0,0 +1,17 @@
+let a=100
+// const b=20
+// var c=30
+
+if(true){
+    let a=10
+    const b=20
+    var c=30
+    console.log("INNER:-",a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+// NOTE => node wala scope different hai window console wala scope se 
+
