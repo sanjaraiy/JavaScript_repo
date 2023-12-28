3192287ebcab60e1866465f3132e2851349bb00f
sanjaraiy <deepakrai78788@gmail.com>
2023-12-28 11:51:42 +0530

    intro_Of_Arrays

---
+++ arrays.js
@@ -0,0 +1,46 @@
+const myArr=[0,1,2,3,5]
+
+const myHeros=["shaktiman","naagraj"]
+
+const myArr2=new Array(1,2,3,4,5)
+
+// console.log(myArr[1]);
+
+
+// Array methods
+const arr=[1,2,3,4,5]
+// arr.push(6);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.unshift(10);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// console.log(arr.includes(9));//return datatype->boolean
+// console.log(arr.indexOf(3));
+
+//---join method on array-------
+// const newArr=arr.join()
+// console.log(arr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//-----diff btw slice V/S splice-----
+//slice=>(1) original array is not manipulated
+      // (2) last index is not included
+
+//splice=>(1) original array is manipulated
+       // (2)  last index is included
+// console.log("A:-",arr);
+// const myn1=arr.slice(1,3);
+// console.log(myn1);
+// console.log("B:-",arr);
+
+// const myn2=arr.splice(1,3);
+// console.log("C:-",arr);
+// console.log(myn2);
+
+
+
+
